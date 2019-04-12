792ff7bf5edf78f34e3e246be198e1d6794259ac
Darren <1240101543@qq.com>
2019-04-12 11:01:12 +0800

    utils

--- spring-boot-utils/src/main/java/com/darren/center/springboot/SpringbootUtilsApplication.java
+++ spring-boot-utils/src/main/java/com/darren/center/springboot/SpringbootUtilsApplication.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootUtilsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootUtilsApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUtilsApplication.class, args);
+	}
+
 
 }
 
