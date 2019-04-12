78184ac21811aac0e3ebd2f51acd99894685d4c7
Darren <1240101543@qq.com>
2019-04-12 10:45:38 +0800

    upload filename

--- spring-boot-distributedlock/src/main/java/com/darren/center/springboot/SpringbootDistributedlockApplication.java
+++ spring-boot-distributedlock/src/main/java/com/darren/center/springboot/SpringbootDistributedlockApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DarrenSpringBootCenterApplication {
+public class SpringbootDistributedlockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DarrenSpringBootCenterApplication.class, args);
+		SpringApplication.run(SpringbootDistributedlockApplication.class, args);
 	}
 
 	@Bean
