11be5b301172364b608727ce987e679802be3f9b
Darren <1240101543@qq.com>
2019-05-24 16:34:08 +0800

    简单使用beetl

--- spring-boot-beetl/src/main/java/com/darren/center/springboot/controller/BaseController.java
+++ spring-boot-beetl/src/main/java/com/darren/center/springboot/controller/BaseController.java
@@ -1,16 +1,22 @@
 package com.darren.center.springboot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
+@Slf4j
 public class BaseController {
 
-    @GetMapping(value = "/")
-    public String index(Model model){
-        model.addAttribute("a", "123456");
-        return "index";
+    @GetMapping("/")
+    public String index(HttpServletRequest request){
+        log.info("index --- begin ---");
+        request.setAttribute("beetl","官网www.ibeetl.com");
+        request.setAttribute("test","springboot 集成 beetl 一起来学呀");
+        log.info("index --- end ---");
+        return "index.btl";
     }
 
 }
